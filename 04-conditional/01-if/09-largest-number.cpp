d51975ccdf21ab5abfa5d384f1fedc6c148a098e
Jakues <jakueenak@gmail.com>
2022-10-25 22:03:33 +0700

    Update : if conditional

---
+++ 04-conditional/01-if/09-largest-number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const string notifAngka = "Angka terbesar = ";
+    float angka1, angka2;
+
+    printf("Program untuk menentukan angka paling besar nilainya.\n");
+    printf("Masukkan angka pertama\t: ");
+    cin >> angka1;
+    printf("Masukkan angka kedua\t: ");
+    cin >> angka2;
+
+    if (angka1 == angka2) {
+        cout << "Angka sama ->  " << angka1 <<  " = " << angka2; 
+    } else if (angka1 >= angka2) {
+        cout << notifAngka << angka1;
+    }
+    else {
+        cout << notifAngka << angka2;
+    }
+}
---
+++ 04-conditional/01-if/10-user-password-protected.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const string errUser        = "Akses ditolak untuk user ";
+    const string errPass        = "Password salah untuk user ";
+	const string acessGranted   = "Login berhasil untuk user ";
+	const string user           = "rill";
+    const string pass           = "2002";
+    string u, p;
+
+    printf("Program Simulasi Login\n");
+    printf("Masukkan user : ");
+    cin >> u;
+    printf("Masukkan password : ");
+    cin >> p;
+
+    if (u != user) {
+        cout << errUser << u;
+    } else if (p != pass) {
+        cout << errPass << u;
+    } else {
+        cout << acessGranted << u;
+    }
+}
