df7628dd29b0ae0a06bbd5adaafbd13f70110de3
Jakues <jakueenak@gmail.com>
2022-10-26 20:49:48 +0700

    Update : switch

---
+++ 04-conditional/02-switch/04-time-conversion.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int menuKonversi, waktu, detik, menit, jam, waktuTemp;
+
+    printf("Program Konversi Waktu\n");
+    printf("1. Konversi waktu detik ke jam-menit-detik\n");
+    printf("2. Konversi waktu menit ke jam-menit-detik\n");
+    printf("3. Konversi waktu jam ke detik\n");
+    printf("4. Exit\n");
+    printf("Pilih Konversi [1-4]: ");
+    cin >> menuKonversi;
+
+    switch (menuKonversi) {
+        case 1:
+            printf("Program untuk konversi detik ke jam-menit-detik.\n");
+            printf("Masukkan waktu dalam detik yang akan dikonversi : ");
+            cin >> waktu;
+
+            waktuTemp = waktu;
+            jam = waktu / 3600;
+            waktu = waktu % 3600;
+            menit = waktu / 60;
+            waktu = waktu % 60;
+            detik = waktu;
+
+            printf("Waktu %d detik dikonversi menjadi %d jam %d menit %d detik\n", waktuTemp, jam, menit, detik);
+            break;
+        case 2:
+            printf("Program untuk konversi menit ke jam-menit-detik.\n");
+            printf("Masukkan waktu dalam menit yang akan dikonversi : ");
+            cin >> waktu;
+
+            waktuTemp = waktu;
+            jam = waktu / 60;
+            waktu = waktu % 60;
+            menit = waktu;
+            detik = waktu / 60;
+
+            printf("Waktu %d menit dikonversi menjadi %d jam %d menit %d detik\n", waktuTemp, jam, menit, detik);
+            break;
+        case 3:
+            float waktuF;
+            printf("Program untuk konversi jam ke detik.\n");
+            printf("Masukkan waktu dalam jam yang akan dikonversi : ");
+            cin >> waktuF;
+
+            waktuTemp = waktuF;
+            detik = waktuF * 3600.;
+
+            printf("Waktu %d jam dikonversi menjadi %d detik\n", waktuTemp, detik);
+        case 4:
+            break;
+        default:
+            printf("Invalid ! Tip : Masukkan 1-4\n");
+            break;
+    }
+}
---
+++ 04-conditional/02-switch/05-meters-conversion.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const string OK=" [OK]";
+    const string INVLD="[!] ";
+    const string INFO="[i] ";
+    const string QST="[?] ";
+    int menuUkuran;
+    float meters, feets, inches, yards, km;
+
+    printf("Program Konversi Ukuran (Meter)\n");
+    printf("1. Konversi meters -> feets\n");
+    printf("2. Konversi feets -> meters\n");
+    printf("3. Konversi meters -> inches\n");
+    printf("4. Konversi inches -> meters\n");
+    printf("5. Konversi meters -> yards\n");
+    printf("6. Konversi yards -> meters\n");
+    printf("7. Konversi meters -> km\n");
+    printf("8. Konversi km -> meters\n");
+    printf("9. Exit\n");
+    printf("Pilih Konversi [1-9]: ");
+    cin >> menuUkuran;
+
+    switch (menuUkuran) {
+        case 1:
+            printf("Konversi meters -> feets\n");
+            printf("Input meters : ");
+            cin >> meters;
+
+            feets = meters / 0.3048;
+
+            printf("%g meters = %g feets\n", meters, feets);
+            break;
+        case 2:
+            printf("Konversi feets -> meters\n");
+            printf("Input feets : ");
+            cin >> feets;
+
+            meters = feets * 0.3048;
+
+            printf("%g feets = %g meters\n", feets, meters);
+            break;
+        case 3:
+            printf("Konversi meters -> inches\n");
+            printf("Input meters : ");
+            cin >> meters;
+
+            inches = meters / 0.0254;
+
+            printf("%g meters = %g inches\n", meters, inches);
+            break;
+        case 4:
+            printf("Konversi inches -> meters\n");
+            printf("Input inches : ");
+            cin >> inches;
+
+            meters = inches * 0.0254;
+            
+            printf("%g inches = %g meters\n", inches, meters);
+            break;
+        case 5:
+            printf("Konversi meters -> yards\n");
+            printf("Input meters : ");
+            cin >> meters;
+
+            yards = meters / 0.9144;
+
+            printf("%g meters = %g yards\n", meters, yards);
+            break;
+        case 6:
+            printf("Konversi yards -> meters\n");
+            printf("Input yards : ");
+            cin >> yards;
+
+            meters = yards * 0.9144;
+
+            printf("%g yards = %g meters\n", yards, meters);
+            break;
+        case 7:
+            printf("Konversi meters -> km\n");
+            printf("Input meters : ");
+            cin >> meters;
+
+            km = meters / 1000;
+
+            printf("%g meters = %g km\n", meters, km);
+            break;
+        case 8:
+            printf("Konversi km -> meters\n");
+            printf("Input km : ");
+            cin >> km;
+
+            meters = km * 1000;
+
+            printf("%g km = %g meters\n", km, meters);
+            break;
+        case 9:
+            break;
+        default:
+            cout << INVLD << "Invalid" << endl;
+            cout << INVLD << "Tip : input 1-9";
+            break;
+    }
+}
