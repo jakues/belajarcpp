59f8452ef2a392e8b0330234ea6d83e30caf998c
Jakues <jakueenak@gmail.com>
2022-11-13 01:02:07 +0700

    Nested Loops: Add conditional

--- 06-nested-loops/02-multiplication-table.cpp
+++ 06-nested-loops/02-multiplication-table.cpp
@@ -8,19 +8,23 @@ int main() {
     printf("Masukkan batas angka : ");
     cin >> max;
     
-    // print tabel ke samping
-    printf("%5s", "X");
-    for (i = 1; i <= max; i++) {
-        printf("%5d", i);
-    }
-
-    // print tabel ke bawah
-    for (i = 1; i <= max; i++) {
-        printf("\n%5d", i);
-        
-        // print hasil perkalian
-        for (j = 1; j <= max; j++){
-            printf("%5d", j*i);
+    if (max > 30) {
+        printf("Batas maksimum = 30");
+    } else {
+        // print tabel ke samping
+        printf("%5s", "X");
+        for (i = 1; i <= max; i++) {
+            printf("%5d", i);
+        }
+    
+        // print tabel ke bawah
+        for (i = 1; i <= max; i++) {
+            printf("\n%5d", i);
+            
+            // print hasil perkalian
+            for (j = 1; j <= max; j++){
+                printf("%5d", j*i);
+            }
         }
     }
 }
