7a5ffc23f3602dfa98b8c9aff683f671a31f7f63
Jakues <jakueenak@gmail.com>
2022-11-24 11:45:47 +0700

    Update license

--- 02-typedata-variable-constant/17-constant-define.cpp
+++ 02-typedata-variable-constant/17-constant-define.cpp
@@ -1,10 +1,11 @@
 // Copyright © 2022 Rill
 // My progress learning on c++
+// License : https://creativecommons.org/licenses/by-nc-sa/4.0/
 // Follow me on twitter : https://twitter.com/rill_blastmith
 
 #include <iostream>
 #define phi 3.14
-#define phiOther float(22)/float(7)
+#define phiOther float(22)/7. // float(num) == num.
 
 using namespace std;
 
--- 04-conditional/01-if/09-largest-number.cpp
+++ 04-conditional/01-if/09-largest-number.cpp
@@ -1,5 +1,6 @@
 // Copyright © 2022 Rill
 // My progress learning on c++
+// License : https://creativecommons.org/licenses/by-nc-sa/4.0/
 // Follow me on twitter : https://twitter.com/rill_blastmith
 
 #include <iostream>
