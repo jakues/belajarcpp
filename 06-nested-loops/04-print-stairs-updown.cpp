459d39a44c26a066e8c85ea33e5529e36d3a3d0a
Jakues <jakueenak@gmail.com>
2022-11-24 11:22:56 +0700

    Nested loops: add copyright

--- 06-nested-loops/04-print-stairs-updown.cpp
+++ 06-nested-loops/04-print-stairs-updown.cpp
@@ -1,3 +1,8 @@
+// Copyright © 2022 Rill
+// My progress learning on c++
+// License : https://creativecommons.org/licenses/by-nc-sa/4.0/
+// Follow me on twitter : https://twitter.com/rill_blastmith
+
 #include <iostream>
 
 using namespace std;
