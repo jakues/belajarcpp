fc80b92e374d365b7032f5e44d14eeccd69e9299
Jakues <jakueenak@gmail.com>
2022-11-28 23:52:32 +0700

    Added function

---
+++ 07-function/02-user-defined-func.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void incN() {
+    N++;
+}
+
+void showN() {
+    if (N == 0)
+        return;
+    printf("showN\t: N is %d\n", N);
+}
+
+int main() {
+    // another basic user defined function
+    // this will print 2 coz function call 2x so
+    // N++; N++ == 0+1; 1+1 == 2
+    showN();
+    incN();
+    incN();
+    showN();
+}
---
+++ 07-function/03-void-func.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void sayHello() {
+    const string msg = "Welcome to Hotel California";
+    const string icon = "[i]";
+    printf("%s %s", data(icon), data(msg));
+}
+
+int main() {
+    sayHello();
+}
