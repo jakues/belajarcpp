2d601da6526278f57123a76f0c18aed58c37830b
Jakues <jakueenak@gmail.com>
2022-12-12 22:06:17 +0700

    Regroup arrays single and multi dimensional

---
+++ 08-arrays/06-empty-array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main() {
+    // declare empty array with size_max = 3
+    array <int, 3> num;
+
+    // here we go
+    if (num.empty() != true) {
+        printf("Array elements is empty\n");
+        // then fill the empty elements
+        int dNum;
+        printf("Input number to fill array : ");
+        cin >> dNum;
+        num.fill(dNum);
+        // print it using for loops
+        printf("Array num[3] : ");
+        for (auto i: num) {
+            printf("%d ", num[i]);
+        }
+    } else {
+        printf("Array elements not empty\n");
+    }
+}
