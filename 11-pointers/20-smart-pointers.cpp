7e64f96fda801ef08f65afb6fa44a5be68a99ff5
Jakues <jakueenak@gmail.com>
2022-12-31 11:27:40 +0700

    Pointers: added smart pointers

---
+++ 11-pointers/20-smart-pointers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+//reference https://en.cppreference.com/w/cpp/header/memory
+#include <memory>
+
+using namespace std;
+
+int main() {
+    // Declare unique pointer
+    unique_ptr<int>Ptr1 = make_unique<int>(3);
+    unique_ptr<float>Ptr2 = make_unique<float>(3.14);
+    unique_ptr<char>Ptr3 = make_unique<char>('P');
+
+    printf("Ptr1 : %-16d address : %-#16p\n", *Ptr1, &Ptr1);
+	printf("Ptr2 : %-16.2f address : %-#16p\n", *Ptr2, &Ptr2);
+	printf("Ptr3 : %-16c address : %-#16p\n", *Ptr3, &Ptr3);
+
+    // Moving unique pointer
+    unique_ptr<int>movePtr = move(Ptr1);
+    printf("\nPtr1 : %-16d address : %-#16p\n", *movePtr, &movePtr);
+    // Ptr1 will not exist
+    // coz ady move to movePtr
+    // try uncomment code below
+    // printf("\nPtr1 : %-16d address : %-#16p\n", *Ptr1, &Ptr1);
+}
