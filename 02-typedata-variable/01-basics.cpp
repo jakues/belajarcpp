5faa5d37ac403f4d2fcce07407ee6356248a8716
Rill <jakueenak@gmail.com>
2023-02-01 14:04:00 +0700

    Rename folder

---
+++ 02-typedata-variable/01-basics.cpp
@@ -0,0 +1,21 @@
+// Copyright © 2022 Rill
+// Progress learning on c++
+// License : https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // int is integer type data
+    // age is variable we declared
+    int age = 20;
+
+    cout << "Age : " << age << " y.o" << endl;
+
+    // Try uncomment code below
+    // n = 20;
+    // cout << n << endl;
+    // this will error coz typedata not known
+    // in cpp u must declare type data and variable
+}
